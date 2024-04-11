67fe6af87fdb5879678792ade86d9b01536d4cde
Boxiang Wang <boxiangw@nvidia.com>
2024-04-11 10:50:05 -0700

    Bug fix for https://nvbugspro.nvidia.com/bug/4598177

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -1254,8 +1254,16 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         if isinstance(self.model, list):
             for model_module in self.model:
                 model_module.eval()
+                for name, module in model_module.named_modules():
+                    if "fused_attention" in name:
+                        if hasattr(module, "training"):
+                            module.training = True
         else:
             self.model.eval()
+            for name, module in self.model.named_modules():
+                if "fused_attention" in name:
+                    if hasattr(module, "training"):
+                        module.training = True
 
         if self.cfg.get('fp8', False):
             first_val_step = self.prev_step_training and not self.training
